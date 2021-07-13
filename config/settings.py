be4dede41ec87a47cf6389c5daae79b8efae61e9
Your Name <jessi@love.localdomain>
2021-07-13 16:55:28 -0700

    commiting to oull new master

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ import os
 import environ
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, False),
+    
 )
 # reading .env file
 environ.Env.read_env()
